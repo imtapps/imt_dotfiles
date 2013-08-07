8a3925296a7c3c94cd5070ed3f92d991b9d8a589
Jarrod <Jarrod.C.Taylor@gmail.com>
2013-08-07 10:36:12 -0500

    Update symlink.sh
    Added a section to remove existing dot files from home directory.

--- Mac/symlink.sh
+++ Mac/symlink.sh
@@ -1,7 +1,22 @@
 ##
 # You must delete all existing *zsh*, *vim* and *tmux* files and directories
-# in your home folder before running this
+# in your home folder for these new files to work
 ##
+rm -rf ~/.tmux
+rm -rf ~/.zsh-custom
+rm -rf ~/.emacs.d
+rm -rf ~/.gemrc
+rm -rf ~/.gitignore
+rm -rf ~/.gitmodules
+rm -rf ~/.gvimrc
+rm -rf ~/.vimrc
+rm -rf ~/.viminfo
+rm -rf ~/.vim
+rm -rf ~/.oh-my-zsh
+rm -rf ~/.slate
+rm -rf ~/.tmux.conf
+rm -rf ~/.zshrc
+rm -rf ~/README
 
 # Symlink the new config files. Assumes files are saved in ~/imt_dotfiles
 ln -s ~/imt_dotfiles/Mac/vim ~/.vim
